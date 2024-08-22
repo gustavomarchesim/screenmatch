9234935cddbf6115028d929a6632614b3a0c7558
gustavomarchesim <gustavomarchesim@outlook.com>
2024-08-22 17:08:49 -0300

    update

--- src/main/java/br/com/gdam/screenmatch/repository/EpisodioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.gdam.screenmatch.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.gdam.screenmatch.model.Episodio;
-
-public interface EpisodioRepository extends JpaRepository<Episodio, Long> {
-
-}
